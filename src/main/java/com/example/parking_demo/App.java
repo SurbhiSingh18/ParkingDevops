9222d8fbdc4e8b0621f3775aadb5d2fef280c485
SurbhiSingh18 <surbhisingh1809@gmail.com>
2026-03-30 00:05:58 +0530

    added maven project

---
+++ src/main/java/com/example/parking_demo/App.java
@@ -0,0 +1,13 @@
+package com.example.parking_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Build Successful - Smart Parking Project");
+    }
+}
